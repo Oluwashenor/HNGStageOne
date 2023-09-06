b27723ae97c0bc3609493e8e4a8ed05677d6de5d
Adesina Adebayo <adeshiname@gmail.com>
2023-09-06 21:42:44 +0100

    Add project files.

---
+++ HNGStageOne/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HNGStageOne.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNGStageOne.Controllers
+{
+	[ApiController]
+//	[Route("[controller]")]
+	public class HomeController : ControllerBase
+	{
+		[AllowAnonymous]
+		[HttpGet("api/{slack_name}/{track}")]
+		public IActionResult Index(string slack_name, string track)
+		{
+			DateTime now = DateTime.Now;
+			var model = new Home
+			{
+				Slack_name = slack_name,
+				Track = track,
+				Status_code = System.Net.HttpStatusCode.OK,
+				Current_day = now.DayOfWeek.ToString(),
+				Utc_time = now,
+
+			};
+			return Ok(model);
+		}
+	}
+}
---
+++ HNGStageOne/Models/Home.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace HNGStageOne.Models
+{
+	public class Home
+	{
+		public string? Slack_name { get; set; }
+		public string? Current_day { get; set; }
+		public DateTime Utc_time { get; set; }
+		public string? Track { get; set; }
+        public string? GitHubFileUrl { get; set; }
+        public string? GitHubRepoUrl { get; set; }
+        public HttpStatusCode Status_code { get; set; }
+    }
+}
